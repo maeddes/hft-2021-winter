48e8bd974fbb1a12c5e2797b9abe3a04007c75d5
Matthias Haeussler <maeddes@gmail.com>
2021-11-12 08:29:28 +0100

    Datademo

---
+++ datademo/src/main/java/com/hftdemo/datademo/DatademoApplication.java
@@ -0,0 +1,13 @@
+package com.hftdemo.datademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatademoApplication.class, args);
+	}
+
+}
