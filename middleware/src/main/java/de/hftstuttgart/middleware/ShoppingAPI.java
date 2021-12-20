82c43f0b4d25537b1a6703c41b3982f12a60da35
Matthias Haeussler <maeddes@gmail.com>
2021-12-20 09:03:38 +0100

    replaced imports

--- middleware/src/main/java/de/hftstuttgart/middleware/ShoppingAPI.java
+++ middleware/src/main/java/de/hftstuttgart/middleware/ShoppingAPI.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+// TODO: Be careful with Auto Import!
+// import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
 @RestController
